ec4aed766c998d5adcd9d8dc9f9f275c6def9115
Alex <jswift888@gmail.com>
2023-02-25 12:49:15 +0300

    CreditPaymentService

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
         int monthlyYear = service.calculate(9.99, 1, 1_000_000);
         int monthlyTwoYears = service.calculate(9.99, 2, 1_000_000);
-        int monthlythreeYears = service.calculate(9.99, 3, 1_000_000);
+        int monthlyThreeYears = service.calculate(9.99, 3, 1_000_000);
 
         System.out.println(monthlyYear);
         System.out.println(monthlyTwoYears);
-        System.out.println(monthlythreeYears);
+        System.out.println(monthlyThreeYears);
     }
 }
